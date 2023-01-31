3e3efc941d7dbf86a486f3677ac9b998a7b3ac8c
Matyash-Oleksander <matyash19831010@gmail.com>
2023-01-31 11:34:51 +0200

    Delete contact.js

--- controllers/contacts/updateContactById.js
+++ controllers/contacts/updateContactById.js
@@ -1,22 +1,9 @@
-// const contactOperations = require("../../models/contacts");
 const { Contact } = require("../../models/contactsModel");
-// const Joi = require("joi");
+
 const { NotFound } = require("http-errors");
 
-// const contactSchema = Joi.object({
-//   name: Joi.string(),
-//   email: Joi.string(),
-//   phone: Joi.string(),
-// });
-
 const updateContactById = async (req, res, next) => {
-  // const { error } = contactSchema.validate(req.body);
-  // if (error) {
-  //   error.status = 404;
-  //   throw error;
-  // }
   const { id } = req.params;
-  // const { name, email, phone } = req.body;
 
   const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
